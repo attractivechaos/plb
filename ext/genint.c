16ed641f531f3794896aedb067ce07227de9b752
Heng Li <lh3@live.co.uk>
2011-04-26 01:09:52 -0400

    simplify

--- ext/genint.c
+++ ext/genint.c
@@ -1,19 +1,15 @@
+/* This program generates 5 million recurrent integers. Each distinct integer
+ * occurs 4 times in average.
+ */
 #include <stdio.h>
 #include <stdlib.h>
-#include <limits.h>
-
-void print_data(int n)
-{
-	int i;
-	srand48(11);
-	for (i = 0; i < n; ++i)
-		printf("%u\n", (unsigned)((lrand48() % (n/4))  * 271828183u));
-}
 
 int main(int argc, char *argv[])
 {
-	int n = 5000000;
+	int i, n = 5000000; // by default, output 5 million integers
 	if (argc > 1) n = atoi(argv[1]);
-	print_data(n);
+	srand48(11);
+	for (i = 0; i < n; ++i)
+		printf("%u\n", (unsigned)((lrand48() % (n/4))  * 271828183u)); // 4 times in average
 	return 0;
 }
