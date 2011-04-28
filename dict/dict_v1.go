ffe2545b37ceeff43603c4bf917e8ac955b7d33d
Heng Li <lh3@live.co.uk>
2011-04-28 11:57:12 -0400

    A new version contributed by Pat

--- dict/dict_v1.go
+++ dict/dict_v1.go
@@ -1,21 +1,26 @@
-package main;
-import("fmt"; "bufio"; "os")
+// Contributed by Pat
+package main
+
+import (
+	"fmt"
+	"bufio"
+	"os"
+)
 
 func main() {
-	r := bufio.NewReader(os.Stdin);
-	h := make(map[string]int);
-	max := 1;
+	r, _ := bufio.NewReaderSize(os.Stdin, 1e6)
+	h := make(map[string]int, 1e6)
+	max := 1
 	for {
-		if l, e := r.ReadString('\n'); e == nil {
-			if v, ok := h[l]; ok {
-				if max < v + 1 { max = v + 1; }
-				h[l] = v + 1;
-			} else {
-				h[l] = 1;
-			}
-		} else {
-			break;
+		l, e := r.ReadString('\n')
+		if e != nil {
+			break
+		}
+		v := h[l] + 1
+		h[l] = v
+		if v > max {
+			max = v
 		}
 	}
-	fmt.Printf("%d\t%d\n", len(h), max);
+	fmt.Printf("%d\t%d\n", len(h), max)
 }
