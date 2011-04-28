20fc6610943f573773bcf49a8e98a12b33a339dc
Heng Li <lh3@live.co.uk>
2011-04-28 11:40:00 -0400

    A better version contributed by leonardo

--- matmul/matmul_v1.d
+++ matmul/matmul_v1.d
@@ -1,44 +1,45 @@
-// Written by Attractive Chaos; distributed under the MIT license
+// Originally written by Attractive Chaos; distributed under the MIT license (D V.2 code)
+// Contributed by leonardo and then modified by Attractive Chaos to remove D 2.0 features
 
-// translated from matmul_v1.java
 import std.stdio, std.string;
 
-double[][] matgen(int n) {
-	double[][] a;
-	double tmp = 1. / n / n;
-	a.length = n;
-	for (int i = 0; i < n; ++i) a[i].length = n;
-	for (int i = 0; i < n; ++i)
-		for (int j = 0; j < n; ++j)
-			a[i][j] = tmp * (i - j) * (i + j);
+double[][] matGen(in int n) {
+	double tmp = 1.0 / n / n;
+	auto a = new double[][](n, n);
+	foreach (int i, row; a)
+		foreach (int j, ref x; row)
+			x = tmp * (i - j) * (i + j);
 	return a;
 }
-double[][] matmul(double[][] a, double[][] b) {
+
+double[][] matMul(in double[][] a, in double[][] b) {
 	int m = a.length, n = a[0].length, p = b[0].length;
-	double[][] x, c;
-	x.length = m; c.length = p;
-	for (int i = 0; i < m; ++i) x[i].length = p;
-	for (int i = 0; i < p; ++i) c[i].length = n;
-	for (int i = 0; i < n; ++i) // transpose
-		for (int j = 0; j < p; ++j)
-			c[j][i] = b[i][j];
-	for (int i = 0; i < m; ++i)
-		for (int j = 0; j < p; ++j) {
+
+	// transpose
+	auto c = new double[][](p, n);
+	foreach (i, brow; b)
+		foreach (j, bx; brow)
+			c[j][i] = bx;
+
+	auto x = new double[][](m, p);
+
+	foreach (i, arow; a)
+		foreach (j, crow; c) {
+			// x[i][j] = std.numeric.dotProduct(arow, crow); // right way
 			double s = 0.0;
-			for (int k = 0; k < n; ++k)
-				s += a[i][k] * c[j][k];
+			foreach (k, arowk; arow)
+				s += arowk * crow[k];
 			x[i][j] = s;
 		}
+
 	return x;
 }
-int main(string[] args) {
+
+void main(in string[] args) {
 	int n = 100;
-	if (args.length >= 2) n = atoi(args[1]);
-	n = n / 2 * 2;
-	double[][] a, b, x;
-	a = matgen(n);
-	b = matgen(n);
-	x = matmul(a, b);
-	writefln(x[n/2][n/2]);
-	return 0;
+	if (args.length >= 2) n = atoi(args[1]) / 2 * 2;
+	auto a = matGen(n);
+	auto b = matGen(n);
+	auto x = matMul(a, b);
+	writefln(x[n / 2][n / 2]);
 }
