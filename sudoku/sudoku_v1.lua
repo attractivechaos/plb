ce5ce9185c29e8c57ca2394aa4727c4c69009565
Heng Li <lh3@live.co.uk>
2011-06-13 23:15:44 -0400

    speed up by reducing array dimenssion

--- sudoku/sudoku_v1.lua
+++ sudoku/sudoku_v1.lua
@@ -33,6 +33,7 @@ end
 sudoku.solve = function(e, s)
 	local z, x, y, n, p, ret, i_ = {}, {}, {}, {}, {}, {}, 0
 	for i = 1, 9 do
+		local c = 0
 		x, y, z[i] = {0, 0, 0}, {0, 0, 0}, {}
 		for j = 0, 80 do
 			if s:byte(j+1) >= 49 and s:byte(j+1) <= 57 then
@@ -44,36 +45,36 @@ sudoku.solve = function(e, s)
 			local ej = e[j]
 			if bit.band(ej[1], x[1]) == x[1] and bit.band(ej[2], x[2]) == x[2] and bit.band(ej[3], x[3]) == x[3]
 					and bit.band(ej[1], y[1]) == 0 and bit.band(ej[2], y[2]) == 0 and bit.band(ej[3], y[3]) == 0 
-			then table.insert(z[i], e[j]) end
+			then z[i][c], z[i][c+1], z[i][c+2], c = e[j][1], e[j][2], e[j][3], c + 3 end
 		end
-		n[i] = bit.bor(bit.lshift(#z[i], 8), i)
+		n[i] = bit.bor(bit.lshift(c/3, 8), i)
 	end
 	table.sort(n)
-	for i = 1, 9 do p[i], n[i] = 0, bit.band(n[i], 0xff) end
+	for i = 1, 9 do p[i], n[i] = -3, bit.band(n[i], 0xff) end
 	x, i_ = {0, 0, 0}, 1
 	while true do
 		while i_ >= 1 and i_ <= 9 do
 			local zi = z[n[i_]]
-			if p[i_] > 0 then
-				local zip = zi[p[i_]]
-				x[1], x[2], x[3] = bit.band(x[1], bit.bnot(zip[1])), bit.band(x[2], bit.bnot(zip[2])), bit.band(x[3], bit.bnot(zip[3]))
+			if p[i_] >= 0 then
+				local t = p[i_]
+				x[1], x[2], x[3] = bit.band(x[1], bit.bnot(zi[t])), bit.band(x[2], bit.bnot(zi[t+1])), bit.band(x[3], bit.bnot(zi[t+2]))
 			end
-			local j_ = #zi + 1
-			for j = p[i_] + 1, #zi do
-				if bit.band(x[1], zi[j][1]) == 0 and bit.band(x[2], zi[j][2]) == 0 and bit.band(x[3], zi[j][3]) == 0 then
+			local j_ = #zi + 2
+			for j = p[i_] + 3, #zi, 3 do
+				if bit.band(x[1], zi[j]) == 0 and bit.band(x[2], zi[j+1]) == 0 and bit.band(x[3], zi[j+2]) == 0 then
 					j_ = j; break
 				end
 			end
 			if j_ <= #zi then
-				x[1], x[2], x[3], p[i_], i_ = bit.bor(x[1], zi[j_][1]), bit.bor(x[2], zi[j_][2]), bit.bor(x[3], zi[j_][3]), j_, i_ + 1
-			else p[i_], i_ = 0, i_ - 1 end
+				x[1], x[2], x[3], p[i_], i_ = bit.bor(x[1], zi[j_]), bit.bor(x[2], zi[j_+1]), bit.bor(x[3], zi[j_+2]), j_, i_ + 1
+			else p[i_], i_ = -3, i_ - 1 end
 		end
 		if i_ < 1 then break end
 		local y = {}
 		for j = 1, 9 do
-			for k = 0, 31 do if bit.band(bit.rshift(z[n[j]][p[j]][1], k), 1) == 1 then y[k+1]  = n[j] end end
-			for k = 0, 31 do if bit.band(bit.rshift(z[n[j]][p[j]][2], k), 1) == 1 then y[k+33] = n[j] end end
-			for k = 0, 16 do if bit.band(bit.rshift(z[n[j]][p[j]][3], k), 1) == 1 then y[k+65] = n[j] end end
+			for k = 0, 31 do if bit.band(bit.rshift(z[n[j]][p[j]+0], k), 1) == 1 then y[k+1]  = n[j] end end
+			for k = 0, 31 do if bit.band(bit.rshift(z[n[j]][p[j]+1], k), 1) == 1 then y[k+33] = n[j] end end
+			for k = 0, 16 do if bit.band(bit.rshift(z[n[j]][p[j]+2], k), 1) == 1 then y[k+65] = n[j] end end
 		end
 		ret[#ret+1] = y
 		i_ = i_ - 1
