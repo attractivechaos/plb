55899a1522c75d6f35a539b636b68552183a565e
Heng Li <lh3@live.co.uk>
2011-06-22 17:21:29 -0400

    fixed a syntax error

--- dict/dict_v1.pl
+++ dict/dict_v1.pl
@@ -1,8 +1,8 @@
 my (%h, $max);
 
 while (<>) {
-    next if ++$h{$_} > $max;
+    next if (++$h{$_} > $max);
     $max = $h{$_};
 }
 
-print keys %h . "\t$max\n";
+print scalar(keys(%h)), "\t$max\n";
